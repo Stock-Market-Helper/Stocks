aa184d297e78d9833e6539cf0830e6a549d2fa25
Mazyoon <95772336+Mazyoon@users.noreply.github.com>
2023-05-30 09:15:45 +0300

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM stock_market.users
+LIMIT 0, 1000
+
+-- Date: 2023-05-30 09:15
+*/
+INSERT INTO `` (`Username`,`Password`,`verification`,`Balance`,`Aramco`,`Google`,`Apple`) VALUES ('abood','arar',1847,937002,4,2,13);
+INSERT INTO `` (`Username`,`Password`,`verification`,`Balance`,`Aramco`,`Google`,`Apple`) VALUES ('Bader','123456789',8529,15000,0,0,0);
+INSERT INTO `` (`Username`,`Password`,`verification`,`Balance`,`Aramco`,`Google`,`Apple`) VALUES ('Mazen','12321',8854,14000,0,0,0);
+INSERT INTO `` (`Username`,`Password`,`verification`,`Balance`,`Aramco`,`Google`,`Apple`) VALUES ('Rayan','102030',4821,80000,10,0,5);
